951a86589ab560117d21065b846b07ed48e1186c
harshitraval112-ux <harshitraval112@gmail.com>
2026-07-04 15:40:44 +0530

    ✅ Java End-of-file | Medium | C++ | Runtime: N/A | Memory: N/A | 2026-07-04

---
+++ hackerrank/medium/java-end-of-file/solution.cpp
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        String fame = sc.nextLine();
+        String game = sc.nextLine();
+        
+        System.out.println("1" + " " + name);
+        System.out.println("2" + " " + fame);
+        System.out.println("3" + " " + game);
+    }
+}
